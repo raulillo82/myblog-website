d2055b088cf70e530548919b548bf466b69c9c3e
Raúl Osuna <raulosuna@gmail.com>
2022-03-22 16:54:00 +0100

    Challenge 8: implement post method for the form to be actually sent somewhere and log it

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
+app.post("/compose", function(req, res) {
+  const item = req.body.input;
+  console.log(item);
+  //res.redirect('/compose');
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
